b3d5cdcf5cd2eeb18328dcb445a7b3206d12b65e
Dan Van Atta <dhvatta@gmail.com>
2016-03-20 14:48:42 -0700

    Update clone_all to iterate over each page result. Github will only return a max  limit of 100 repos. We'll need to update this again when we break 200 map repos to iterate over a third result page.

--- bin/clone_all.sh
+++ bin/clone_all.sh
@@ -4,13 +4,18 @@
  ## note:
  ##  -  it appears that it takes some time for a repo to show up in an orgs repo list
  ##  -  the curl call below is rate limited, not intended for repeated invocations
-for i in $(curl --silent "https://api.github.com/orgs/triplea-maps/repos?page=1&per_page=10000" | grep git_url | sed 's/.*: "//i' | sed 's/",$//'); do
- mapName=$(echo $i | sed 's/.git$//' | sed 's|.*/||')
 
- echo $mapName - $i
- if [ ! -d "$mapName" ]; then
-   git clone $i
- fi
+for j in 1 2;
+do
+  for i in $(curl --silent "https://api.github.com/orgs/triplea-maps/repos?page=$j&per_page=1000" | grep git_url | sed 's/.*: "//i' | sed 's/",$//');
+  do
+    mapName=$(echo $i | sed 's/.git$//' | sed 's|.*/||')
+
+    echo $mapName - $i
+    if [ ! -d "$mapName" ]; then
+     git clone $i
+   fi
+  done
 done
 
 
