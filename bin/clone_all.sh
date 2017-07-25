5447dc5245b47ca89df0c04667ad86b0000f056c
Dan Van Atta <dhvatta@gmail.com>
2017-07-24 20:38:31 -0700

    Shorten clone_all (#85)
    * Shorten clone_all

--- bin/clone_all.sh
+++ bin/clone_all.sh
@@ -5,17 +5,11 @@
  ##  -  it appears that it takes some time for a repo to show up in an orgs repo list
  ##  -  the curl call below is rate limited, not intended for repeated invocations
 
-for j in 1 2;
-do
-  for i in $(curl --silent "https://api.github.com/orgs/triplea-maps/repos?page=$j&per_page=1000" | grep git_url | sed 's/.*: "//i' | sed 's/",$//');
-  do
-    mapName=$(echo $i | sed 's/.git$//' | sed 's|.*/||')
-
-    echo $mapName - $i
-    if [ ! -d "$mapName" ]; then
-     git clone $i
-   fi
-  done
+for j in 1 2; do
+  while read gitUrl; do 
+    echo "clone $gitUrl"; 
+    git clone $gitUrl; 
+  done < <(curl --silent "https://api.github.com/orgs/triplea-maps/repos?page=$j&per_page=1000" | grep git_url  | sed 's/.*git:/git:/' | sed 's/",//')
 done
 
 
